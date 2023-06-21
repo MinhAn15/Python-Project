944b0a75332793ab1c1f4a81f1227d39b05482a8
minhan <minhanbk5@gmail.com>
2023-06-21 18:35:49 +0700

    finish simple app PDF creator

--- 6. PDF creator/main.py
+++ 6. PDF creator/main.py
@@ -1,11 +1,16 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
+df = pd.read_csv("topics.csv")
 
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello World", align="L", ln=1, border=1)
-pdf.cell(w=0, h=12, txt="How are you?", align="L", ln=1, border=1)
+for index, row in df.iterrows():
+    pdf.add_page()
+    
+    pdf.set_font(family="Times", style="B", size=15)
+    pdf.set_text_color(102,11,100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
+    pdf.line(10,22,200,22)
 
 pdf.output("output.pdf")
